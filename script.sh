66e7b42e4b227d0c35948798d580b0b71d87155b
Nemesis Contreras <nemesisc96@gmail.com>
2018-03-24 08:48:51 -0700

    Add files via upload

---
+++ script.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+
+#checking the current Apple version of Git
+
+function version_git {
+		echo "This is the version of Apples Git:" 
+		git --version
+		sleep 1
+		echo "This is the path to it"
+		which git
+}
+
+version_git
+
+
+#If Homebrew is not installed then install it
+
+if test ! $(which brew); then
+	echo "Installing Homebrew..."
+	sleep 1
+	ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+fi
+
+#Installing Git via Homebrew
+echo "now installing git....."
+brew install git 
+
+#Replacing Apple git distro with the local one installed
+function set_path{
+		echo "Setting path to the local git distro instead of the Apple one...."
+		export PATH=/usr/local/bin:$PATH
+		echo "The current version should now be:"
+		git --version
+}
+
+set_path
+sleep 1
+echo "to update git in the future run: brew upgrade git"
+echo "bye bye (^._.^)ﾉ"
+
+
+
+
+
